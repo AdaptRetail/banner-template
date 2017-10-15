b145bac632aa639fb16f4a9c5383900e652ce037
Lasse <lasse@haslev.no>
2017-10-15 20:33:31 +0200

    Switched AdaptData to LightAdaptData when running production script

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,6 +5,20 @@ let mix = require( 'laravel-mix' );
  */
 require( '@adapt-retail/adapt-mix-extender' ).extend( mix );
 
+/**
+ * Switch banner data to light mode
+ * when we should publish banner to Adapt
+ */
+if (mix.inProduction()) {
+    mix.webpackConfig({
+        resolve: {
+            alias: {
+                '@adapt-retail/banner-data': '@adapt-retail/banner-data/dist/LightAdaptData.js',
+            }
+        }
+    });
+}
+
 /*
  * Build script and compile sass
  * Display banner and hot reload
